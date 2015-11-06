cbb3be2e96e282659a2f66ecaf2ed2513c33f96b
Raymond Wu <chihwu@bu.edu>
2015-11-06 00:10:26 -0500

    Added Bootstrap for styling

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
+# for the things users can do, but not should do, use the RSpec controller test rather than a feature test
 RSpec.describe ProjectsController, type: :controller do
 	it "handles a missing project correctly" do
-		get :show, id: "not-here"
+		get :show, id: "not-here" # make a GET request to the show action in the ProjectsController with the passed-in id being "not-here"
 
 		expect(response).to redirect_to(projects_path)
 
