fa4f87947812f406dc1b83014fa43c46892edf5f
Raymond Wu <chihwu@bu.edu>
2015-11-06 20:36:44 -0500

    Cull tickets when project gets destroyed

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-	has_many :tickets
+	has_many :tickets, dependent: :delete_all #delete the tickets belonging the ptoject when it is deleted
 	
 	validates :name, presence: true
 end
